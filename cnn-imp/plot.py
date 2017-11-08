15ce06710a4127e97a4031461619b6c7baaca0e0
Amadeus Chan <greenclouds@foxmail.com>
2017-11-08 17:15:58 +0800

    S

---
+++ cnn-imp/plot.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_valid(name):
+    with open("result/" + name, "r") as f:
+        epoch = []
+        loss = []
+        acc = []
+        while True:
+            line = f.readline()
+            if line == None or len(line) == 0:
+                break
+            z = line.split(" ")
+            epoch.append(int(z[0]))
+            loss.append(float(z[1]))
+            acc.append(float(z[2]))
+
+        p1 = plt.subplot(1, 2, 1)
+
+        plt.plot(epoch, loss)
+        plt.xlabel("epoch")
+        plt.ylabel("validation loss")
+
+        p2 = plt.subplot(1, 2, 2)
+        plt.plot(epoch, acc)
+        plt.xlabel("epoch")
+        plt.ylabel("validation accuracy")
+
+        plt.show()
+
+def plot_train(name):
+    with open("result/" + name, "r") as f:
+        epoch = []
+        loss = []
+        acc = []
+
+        loss_ = []
+        acc_ = []
+        while True:
+            line = f.readline()
+            if line == None or len(line) == 0:
+                break
+            z = line.split(" ")
+            e = int(z[0])
+            loss_.append(float(z[1]))
+            acc_.append(float(z[2]))
+            if e % 100 == 0:
+                epoch.append(e)
+                loss.append(np.mean(loss_))
+                acc.append(np.mean(acc_))
+                loss_ = []
+                acc_ = []
+
+        p1 = plt.subplot(1, 2, 1)
+
+        plt.plot(epoch, loss)
+        plt.xlabel("epoch")
+        plt.ylabel("training loss")
+
+        p2 = plt.subplot(1, 2, 2)
+        plt.plot(epoch, acc)
+        plt.xlabel("epoch")
+        plt.ylabel("training accuracy")
+
+        plt.show()
+
+plot_train("train_d.txt")
--- hw3-cnn/plot.py
+++ hw3-cnn/plot.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def read_valid(name):
+    with open("result/" + name, "r") as f:
+        epoch = []
+        loss = []
+        acc = []
+        while True:
+            line = f.readline()
+            if line == None or len(line) == 0:
+                break
+            z = line.split(" ")
+            epoch.append(int(z[0]))
+            loss.append(float(z[1]))
+            acc.append(float(z[2]))
+    return epoch, loss, acc
+
 def plot_valid(name):
     with open("result/" + name, "r") as f:
         epoch = []
@@ -28,6 +43,31 @@ def plot_valid(name):
 
         plt.show()
 
+def read_train(name):
+    with open("result/" + name, "r") as f:
+        epoch = []
+        loss = []
+        acc = []
+
+        loss_ = []
+        acc_ = []
+        while True:
+            line = f.readline()
+            if line == None or len(line) == 0:
+                break
+            z = line.split(" ")
+            e = int(z[0])
+            loss_.append(float(z[1]))
+            acc_.append(float(z[2]))
+            if e % 100 == 0:
+                epoch.append(e)
+                loss.append(np.mean(loss_))
+                acc.append(np.mean(acc_))
+                loss_ = []
+                acc_ = []
+    return epoch, loss, acc
+
+
 def plot_train(name):
     with open("result/" + name, "r") as f:
         epoch = []
@@ -54,13 +94,33 @@ def plot_train(name):
         p1 = plt.subplot(1, 2, 1)
 
         plt.plot(epoch, loss)
-        plt.xlabel("epoch")
+        plt.xlabel("iteration")
         plt.ylabel("training loss")
 
         p2 = plt.subplot(1, 2, 2)
         plt.plot(epoch, acc)
-        plt.xlabel("epoch")
+        plt.xlabel("iteration")
         plt.ylabel("training accuracy")
 
         plt.show()
 
+p1 = plt.subplot(1, 2, 1)
+plt.xlabel("iteration")
+plt.ylabel("loss")
+
+p2 = plt.subplot(1, 2, 2)
+plt.xlabel("iteration")
+plt.ylabel("accuracy")
+
+index, loss, acc = read_train("train_nobn.txt")
+p1.plot(index, loss, label = "without batch normalization")
+p2.plot(index, acc, label = "without batch normalization")
+
+index, loss, acc = read_train("train_bn.txt")
+p1.plot(index, loss, label = "with batch normalization")
+p2.plot(index, acc, label = "with batch normalization")
+
+p1.legend(loc='upper right')
+p2.legend(loc='lower right')
+
+plt.show()
